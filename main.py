e5e02ae9e312b2a3a83afac9590ace305c41cc79
yus04 <yuswim192@gmail.com>
2023-09-15 16:10:44 +0900

    refactor main.py

--- main.py
+++ main.py
@@ -1,39 +1,57 @@
 import asyncio
 import semantic_kernel as sk
-from semantic_kernel.connectors.ai.open_ai import AzureTextCompletion
 from semantic_kernel.connectors.ai.open_ai import AzureChatCompletion
 from semantic_kernel.planning.basic_planner import BasicPlanner
+from semantic_kernel.planning.basic_planner import Plan
 from skills.GetStockPrice.native_function import GetStockPrice
 from skills.PredictStockPrice.native_function import PredictStockPrice
 
-async def main():
-    # カーネルの作成
+# カーネルを作成する関数
+def create_kernel() -> sk.Kernel:
     kernel = sk.Kernel()    
     deployment, api_key, endpoint = sk.azure_openai_settings_from_dot_env()
     completion_service = AzureChatCompletion(deployment, endpoint, api_key)
     kernel.add_chat_service("gpt-35-turbo", completion_service)
+    return kernel
 
-    # スキルの読み込み
+# カーネルのスキルを読み込み
+def load_skills(kernel: sk.kernel) -> sk.Kernel:
     skill_dir = "skills"
     analyze_skill = kernel.import_semantic_skill_from_directory(skill_dir, "AnalyzeReason")
     get_skill = kernel.import_skill(GetStockPrice(), skill_name="GetStockPrice")
     predict_skill = kernel.import_skill(PredictStockPrice(), skill_name="PredictStockPrice")
+    return kernel
 
-    # ユーザの入力を基に実行計画を作成
+async def create_plan(planner: BasicPlanner, kernel: sk.Kernel, use_my_prompt: bool, user_input: str, my_prompt: str) -> Plan:
+    if use_my_prompt:
+        return await planner.create_plan_async(user_input, kernel, my_prompt)
+    else:
+        return await planner.create_plan_async(user_input, kernel)
+
+async def execute_plan(planer: BasicPlanner, plan: Plan, kernel: sk.Kernel) -> str:
+    response = await planer.execute_plan_async(plan, kernel)
+    return response
+
+async def main():
+    kernel = create_kernel()
+    kernel = load_skills(kernel)
+
+    use_my_prompt = False
+
     user_input = """
     Get Microsoft's stock price for the last one month,
     If the stock price is likely to rise or fall in the future, you can output a "Buy" or a "Sell",
     Please also tell us why you made this decision.
     """
-    planner = BasicPlanner()
-    # my_prompt = """
-    # Original promplt is here.
-    # """
-    # plan = await planner.create_plan_async(user_input, kernel, my_prompt)
-    plan = await planner.create_plan_async(user_input, kernel)
 
-    # プランを実行
-    response = await planner.execute_plan_async(plan, kernel)
+    my_prompt = """
+    You can define any promplt here.
+    """
+
+    planner = BasicPlanner()
+    plan = await create_plan(planner, kernel, use_my_prompt, user_input, my_prompt)
+    response = await execute_plan(planner, plan, kernel)
     print(response)
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
